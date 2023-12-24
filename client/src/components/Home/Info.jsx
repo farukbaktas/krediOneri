bf14b371260d215dd1c3da4d38c3bc9d1694ae96
Nil Deniz Alşan <nildenizalsan.53@gmail.com>
2023-12-24 18:07:20 +0300

    24.12

--- client/src/components/Home/Info.jsx
+++ client/src/components/Home/Info.jsx
@@ -2,7 +2,43 @@ import React from 'react'
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <section>
+      {/* info kutucuğu*/}
+      <div className='flex flex-col'>
+        <div className='my-auto mx-36 py-12 px-2'>
+          <div className='shadow-indigo-500/50 shadow-md border rounded-2xl border-purple-500'>
+            <div className='bg-white-50 h-60 rounded-2xl'>              
+              <div className='flex'>
+                {/* sol info*/}
+                <div className='w-1/2 px-1'>
+                  <div className='flex flex-col items-center justify-center h-60'>
+                    
+                    <div class="grid grid-rows-4 grid-flow-col">                                        
+                      <div>01</div>
+                      <div>01</div>
+                      <div>01</div>
+                      <div>01</div>
+                    </div>                                      
+                  </div>
+                </div>
+                
+                {/* sağ info*/}
+                <div className='w-1/2 px-1'>
+                  <div className='flex flex-col items-center justify-center h-60'>
+                    <div class="grid grid-rows-4 grid-flow-col">                                        
+                      <div>01</div>
+                      <div>01</div>
+                      <div>01</div>
+                      <div>01</div>
+                    </div>                                    
+                  </div>
+                </div>                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
